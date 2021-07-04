08d382f5d05fee5cc93b93710af9d344aa215dc2
nicolo.frison <nicolo.frison97@gmail.com>
2021-07-04 13:30:44 +0200

    [front-end] Fix unused declarations

--- src/SignUp/SignUpGroup.tsx
+++ src/SignUp/SignUpGroup.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import SignUp from './SignUp';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const styles = (theme: any) => createStyles({
   paper: {
@@ -47,8 +25,6 @@ const styles = (theme: any) => createStyles({
     margin: theme.spacing(3, 0, 2),
   },
 });
-
-type DOMEvent = keyof GlobalEventHandlersEventMap;
 
 enum FormType {
   create,
