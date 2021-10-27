d497efcbb8ee6ea1157b4373877deed09cbca51a
Nicolo` <nicolo.frison97@gmail.com>
2021-10-27 13:02:13 +0200

    [back-end] Added get orders query parameters

--- backend/src/controllers/orders.controller.ts
+++ backend/src/controllers/orders.controller.ts
@@ -1,6 +1,6 @@
 /* eslint no-console: ["error", { allow: ["info", "error"] }] */
 import * as express from "express";
-import { param } from "express-validator";
+import { Meta, param, query } from "express-validator";
 import Controller from "../interfaces/controller.interface";
 import validationMiddleware from "../middlewares/validation.middleware";
 import HttpException from "../exceptions/HttpException";
@@ -11,6 +11,7 @@ import RequestWithUser from "../interfaces/requestWithUser.interface";
 import OrderPut from "../interfaces/orderPut.interface";
 import OrderAlreadyConfirmedException from "../exceptions/OrderAlreadyConfirmedException";
 import OrderDoesNotExistsException from "../exceptions/OrderDoesNotExistsException";
+import GroupDoesNotExistsException from "../exceptions/GroupDoesNotExistsException";
 
 class OrdersController implements Controller {
   public path = "/orders";
@@ -22,11 +23,36 @@ class OrdersController implements Controller {
   constructor() {
     this.initializeRoutes();
   }
+  
+  private async queryGroupIdMiddleware(
+    groupId: number,
+    meta: Meta
+  ) {
+    if (groupId && (!meta.req.user.groupId || meta.req.user.groupId !== groupId)) {
+      throw new GroupDoesNotExistsException();
+    } else {
+      return true;
+    }
+  }
+
+  private async queryUserIdMiddleware(
+    userId: number,
+    meta: Meta
+  ) {
+    if (userId && (!meta.req.user.userId || meta.req.user.userId !== userId)) {
+      throw new GroupDoesNotExistsException();
+    } else {
+      return true;
+    }
+  }
 
   private initializeRoutes() {
     this.router
       .all(`${this.path}`, authMiddleware)
-      .get(`${this.path}`, this.getOrders)
+      .get(`${this.path}`, 
+        query("groupId").optional().isInt().custom(this.queryGroupIdMiddleware), 
+        query("userId").optional().isInt().custom(this.queryUserIdMiddleware), 
+        this.getOrders)
       .get(`${this.path}/:id`, param("id").isInt(), this.getOrder)
       .delete(`${this.path}/:id`, param("id").isInt(), this.deleteOrder)
       .post(`${this.path}`, validationMiddleware(OrderPost), this.createOrder)
@@ -99,10 +125,18 @@ class OrdersController implements Controller {
     next: express.NextFunction
   ) => {
     try {
+      const groupId = +request.query.groupId;
+      const userId = +request.query.userId;
+
+      let orders = [];
+      if (userId) {
+        orders = await this.orderService.readOrdersByUserId(userId);
+      } else {
+        orders = await this.orderService.readOrdersByGroupId(request.user.groupId);
+      }
+      
       response.status(200);
-      response.json(
-        await this.orderService.readOrdersByUserId(request.user.userId)
-      );
+      response.json(orders);
     } catch (e) {
       console.error(e);
       next(new HttpException(500, "Internal server error"));
