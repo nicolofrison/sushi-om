30aa1802ce1b55f07b98441b945e2ba6585e60da
Nicolo` <nicolo.frison97@gmail.com>
2021-10-28 13:20:39 +0200

    [front-end] Added ThemeProvider in Main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,33 @@
 import React, {Suspense} from 'react';
+
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
+
 import './App.css';
 import Orders from './Orders/Orders';
 import SignUp from './SignUp/SignUp';
 import TranslationSelection from './Utils/TranslationSelection';
 import UserUtils from './Utils/UserUtils';
 
+const theme = createMuiTheme();
+
+const useStyles = makeStyles((theme) => {
+  root: {
+    // some css that access to theme
+  }
+});
+
 function App() {
   return (
-    <Suspense fallback="loading">
-      <div className="App">
-        <header className="App-header">
-          <TranslationSelection />
-          <Content />
-        </header>
-      </div>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <Suspense fallback="loading">
+        <div className="App">
+          <header className="App-header">
+            <TranslationSelection />
+            <Content />
+          </header>
+        </div>
+      </Suspense>
+    </ThemeProvider>
   );
 }
 
