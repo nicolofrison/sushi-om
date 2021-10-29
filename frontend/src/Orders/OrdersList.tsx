8d2099871b94cb9be3fcfea86416ab7fd8b9ddc4
Nicolo` <nicolo.frison97@gmail.com>
2021-10-29 14:47:19 +0200

    [front-end] Added loading spinner for Orders loads and removed edit button

--- frontend/src/Orders/OrdersList.tsx
+++ frontend/src/Orders/OrdersList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {WithTranslation, withTranslation} from "react-i18next";
 
+import CircularProgress from '@mui/material/CircularProgress';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -21,6 +22,7 @@ import AddOrder from './AddOrder';
 import EditOrder from './EditOrder';
 import { handleError, ToFirstCapitalLetter } from '../Utils/Utils';
 import alertService from '../services/alert.service';
+import { AxiosError } from 'axios';
 
 interface IProps extends WithTranslation {
   OrdersType: OrdersType
@@ -28,7 +30,7 @@ interface IProps extends WithTranslation {
 
 interface IState {
   orders: any[],
-  editingOrderId: number | null
+  isLoading: boolean
 }
 
 class OrdersList extends React.Component<IProps, IState> {
@@ -37,7 +39,7 @@ class OrdersList extends React.Component<IProps, IState> {
     super(props);
     this.state = {
       orders: [],
-      editingOrderId: null
+      isLoading: false
     };
   }
 
@@ -59,16 +61,21 @@ class OrdersList extends React.Component<IProps, IState> {
         return;
       }
 
+      this.setState({isLoading: true});
       const userId = this.props.OrdersType === OrdersType.user ? user.userId : -1;
       const groupId = this.props.OrdersType === OrdersType.group ? user.groupId : -1;
 
       OrderService.getOrders(user.accessToken, groupId, userId)
       .then(res => {
-        this.setState({orders: res.data as any[]});
+        this.setState({orders: res.data as any[], isLoading: false});
 
         console.log(res.data);
       })
-      .catch(handleError);
+      .catch((error: Error | AxiosError) => {
+        handleError(error);
+
+        this.setState({isLoading: false});
+      });
     }
 
     deleteOrder(orderId: number) {
@@ -99,6 +106,28 @@ class OrdersList extends React.Component<IProps, IState> {
       }
     }
 
+    tableContent() {
+      return this.state.orders.map((row) => (
+        <TableRow key={row.orderId}>
+            <TableCell component="th" scope="row">
+              
+            </TableCell>
+            <TableCell align="center">{row.code}</TableCell>
+            <TableCell align="center">
+            {this.props.OrdersType === OrdersType.user ?
+              <EditOrder orderId={row.orderId} amount={row.amount} updateOrders={this.updateOrders.bind(this)} />
+              : row.amount }
+            </TableCell>
+            <TableCell align="center">{row.round}</TableCell>
+            <TableCell align="center">
+              {this.props.OrdersType === OrdersType.user ?
+                    <Button size="large" variant="contained" color="error" onClick={() => this.deleteOrder(row.orderId)}>{"Remove"}</Button>
+              : ""}
+            </TableCell>
+        </TableRow>
+        ));
+    }
+
     render() {
       const { t } = this.props;
         return (
@@ -115,32 +144,7 @@ class OrdersList extends React.Component<IProps, IState> {
                 </TableHead>
                 <TableBody>
                     {this.props.OrdersType === OrdersType.user ? <AddOrder updateOrders={this.updateOrders.bind(this)} /> : ""}
-                    {this.state.orders.map((row) => (
-                    <TableRow key={row.orderId}>
-                        <TableCell component="th" scope="row">
-                          
-                        </TableCell>
-                        <TableCell align="center">{row.code}</TableCell>
-                        <TableCell align="center">
-                        {this.props.OrdersType === OrdersType.user ?
-                          <EditOrder orderId={row.orderId} amount={row.amount} updateOrders={this.updateOrders.bind(this)} />
-                          : row.amount }
-                        </TableCell>
-                        <TableCell align="center">{row.round}</TableCell>
-                        <TableCell align="center">
-                          {this.props.OrdersType === OrdersType.user ?
-                            <Grid container spacing={2}>
-                              <Grid item xs={6}>
-                                <Button size="large" variant="contained" color="warning" onClick={() => this.setState.bind(this, row.orderId)}>{"Edit"}</Button>
-                              </Grid> 
-                              <Grid item xs={6}>
-                                <Button size="large" variant="contained" color="error" onClick={() => this.deleteOrder(row.orderId)}>{"Remove"}</Button>
-                              </Grid> 
-                            </Grid>
-                          : ""}
-                        </TableCell>
-                    </TableRow>
-                    ))}
+                    {this.state.isLoading ? <TableRow><TableCell colSpan={5} align="center"><CircularProgress /></TableCell></TableRow> : this.tableContent()}
                 </TableBody>
                 </Table>
             </TableContainer>
