ed166df0c896c2516d4e18f86d63946e7c75893f
Nicolo` <nicolo.frison97@gmail.com>
2021-11-12 17:28:53 +0100

    [backend] Changed database environment variables and fixed validation

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -8,8 +8,20 @@ export default function validateEnv() {
     PORT: port(),
     JWT_SECRET: str(),
     JWT_EXPIRATION: str(),
-    MONGO_PASSWORD: str(),
-    MONGO_PATH: str(),
-    MONGO_USER: str(),
+    PGHOST: str(),
+    PGPORT: num(),
+    PGUSER: str(),
+    PGPASSWORD: str(),
+    PGDATABASE: str()
   });
 }
+
+POSTGRES_HOST=192.168.1.3
+POSTGRES_PORT=5432
+POSTGRES_USER=admin
+POSTGRES_PASSWORD=admin
+POSTGRES_DB=tutorial
+
+PORT=5000
+JWT_SECRET=test
+JWT_EXPIRATION=10d
