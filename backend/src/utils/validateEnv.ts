d95f0720f9efe8bc85d69c86e2bbadb10eab4126
Nicolo` <nicolo.frison97@gmail.com>
2021-11-12 20:26:17 +0100

    [backend] Fixed validateEnv

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -9,19 +9,9 @@ export default function validateEnv() {
     JWT_SECRET: str(),
     JWT_EXPIRATION: str(),
     PGHOST: str(),
-    PGPORT: num(),
+    PGPORT: port(),
     PGUSER: str(),
     PGPASSWORD: str(),
     PGDATABASE: str()
   });
 }
-
-POSTGRES_HOST=192.168.1.3
-POSTGRES_PORT=5432
-POSTGRES_USER=admin
-POSTGRES_PASSWORD=admin
-POSTGRES_DB=tutorial
-
-PORT=5000
-JWT_SECRET=test
-JWT_EXPIRATION=10d
